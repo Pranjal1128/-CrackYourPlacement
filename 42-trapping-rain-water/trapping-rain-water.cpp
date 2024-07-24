8948c17891e540e9d2c3894e5af4c397eec0330e
Pranjal1128 <123015023+Pranjal1128@users.noreply.github.com>
2024-07-24 18:52:16 +0530

    Time: 27 ms (5.68%) | Memory: 24.3 MB (5.31%) - LeetSync

---
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<int> PGE(vector<int> height) {
+        int n = height.size();
+        vector<int> ans(n, -1);
+        for(int i = 1; i < n; i++) {
+           ans[i] = max(ans[i - 1], height[i - 1]) > height[i] ? max(ans[i - 1], height[i - 1]) : -1;
+        }
+
+        return ans;
+    }
+
+    vector<int> NGE(vector<int> height) {
+        int n = height.size();
+        vector<int> ans(n, -1);
+        for(int i = n - 2; i >= 0; i--) {
+            ans[i] = max(ans[i + 1], height[i + 1]) > height[i] ? max(ans[i + 1], height[i + 1]) : -1;
+        }
+
+        return ans;
+    }
+
+    int trap(vector<int>& height) {
+        int n = height.size();
+        vector<int> pge = PGE(height); 
+        vector<int> nge = NGE(height); 
+        for(int i = 0; i < n; i++) cout << pge[i] << " ";
+        cout << endl;
+        for(int i = 0; i < n; i++) cout << nge[i] << " ";
+        cout << endl;
+
+        int ans = 0, i = 0;
+        while(i < n) {
+           if(pge[i] != -1 && nge[i] != -1) {
+                int h = min(pge[i], nge[i]);
+                ans += (h - height[i]);
+           }
+           i++;
+        }
+
+        return ans;
+    }
+};
