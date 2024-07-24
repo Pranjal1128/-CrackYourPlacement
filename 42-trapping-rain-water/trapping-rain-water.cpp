0ae7863a0babb6ba66ba39e68bc4accebe8d1e01
Pranjal1128 <123015023+Pranjal1128@users.noreply.github.com>
2024-07-24 18:55:20 +0530

    Time: 11 ms (52.84%) | Memory: 24.2 MB (5.31%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
-    vector<int> PGE(vector<int> height) {
-        int n = height.size();
+    vector<int> PGE(vector<int> height, int n) {
         vector<int> ans(n, -1);
         for(int i = 1; i < n; i++) {
            ans[i] = max(ans[i - 1], height[i - 1]) > height[i] ? max(ans[i - 1], height[i - 1]) : -1;
@@ -10,8 +9,7 @@ public:
         return ans;
     }
 
-    vector<int> NGE(vector<int> height) {
-        int n = height.size();
+    vector<int> NGE(vector<int> height, int n) {
         vector<int> ans(n, -1);
         for(int i = n - 2; i >= 0; i--) {
             ans[i] = max(ans[i + 1], height[i + 1]) > height[i] ? max(ans[i + 1], height[i + 1]) : -1;
@@ -22,13 +20,8 @@ public:
 
     int trap(vector<int>& height) {
         int n = height.size();
-        vector<int> pge = PGE(height); 
-        vector<int> nge = NGE(height); 
-        for(int i = 0; i < n; i++) cout << pge[i] << " ";
-        cout << endl;
-        for(int i = 0; i < n; i++) cout << nge[i] << " ";
-        cout << endl;
-
+        vector<int> pge = PGE(height, n); 
+        vector<int> nge = NGE(height, n);
         int ans = 0, i = 0;
         while(i < n) {
            if(pge[i] != -1 && nge[i] != -1) {
