bb3820b0f71f346f4941c5e664c6ff025a13cb06
Pranjal Naman <123015023+Pranjal1128@users.noreply.github.com>
2024-07-27 17:45:03 +0530

    Time: 9 ms (86.79%) | Memory: 13.3 MB (59.87%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
@@ -1,19 +1,32 @@
 class Solution {
-public:  
+public:
     long long subArrayRanges(vector<int>& nums) {
-        int mx = INT_MIN, mn = INT_MIN;
         int n = nums.size();
-        long long ans = 0;
-        for(int i = 0; i < n; i++) {
-            mx = nums[i];
-            mn = nums[i];
-            for(int j = i; j < n; j++) {
-                mx = max(mx, nums[j]);
-                mn = min(mn, nums[j]);
-                ans += (mx - mn);
+        long long answer = 0;
+        stack<int> stk;
+        
+        // Find the sum of all the minimum.
+        for (int right = 0; right <= n; ++right) {
+            while (!stk.empty() && (right == n || nums[stk.top()] >= nums[right])) {
+                int mid = stk.top();
+                stk.pop();
+                int left = stk.empty() ? -1 : stk.top();
+                answer -= (long long)nums[mid] * (right - mid) * (mid - left);
             }
+            stk.push(right); 
         }
-
-        return ans;
+        
+        // Find the sum of all the maximum.
+        stk.pop();
+        for (int right = 0; right <= n; ++right) {
+            while (!stk.empty() && (right == n || nums[stk.top()] <= nums[right])) {
+                int mid = stk.top();
+                stk.pop();
+                int left = stk.empty() ? -1 : stk.top();
+                answer += (long long)nums[mid] * (right - mid) * (mid - left);
+            }
+            stk.push(right); 
+        }
+        return answer;
     }
-};  
+};
