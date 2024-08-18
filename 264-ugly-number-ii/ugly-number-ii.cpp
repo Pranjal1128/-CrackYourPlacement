75f87ea2e2a4e44c6bad45c2c991d50afb321aec
Pranjal Naman <123015023+Pranjal1128@users.noreply.github.com>
2024-08-19 00:36:05 +0530

    Time: 7 ms (51.38%) | Memory: 9.7 MB (72.38%) - LeetSync

--- 264-ugly-number-ii/ugly-number-ii.cpp
+++ 264-ugly-number-ii/ugly-number-ii.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int nthUglyNumber(int n) {
-        vector<long long> dp(n + 1, INT_MAX);
-        dp[0] = dp[1] = 1;
-        int cnt2 = 1, cnt3 = 1, cnt5 = 1;
+        int cnt_2 = 1, cnt_3 = 1, cnt_5 = 1;
+        vector<int> dp(n + 1, 1);
         for(int i = 2; i <= n; i++) {
-            int temp = min(dp[cnt2]*2, min(dp[cnt3]*3, dp[cnt5]*5));
-            if(temp == dp[cnt2]*2) cnt2++;
-            if(temp == dp[cnt3]*3) cnt3++;
-            if(temp == dp[cnt5]*5) cnt5++;
+            int temp = min(2*dp[cnt_2], min(3*dp[cnt_3], 5*dp[cnt_5]));
+            if(temp == 2*dp[cnt_2]) cnt_2++;
+            if(temp == 3*dp[cnt_3]) cnt_3++;
+            if(temp == 5*dp[cnt_5]) cnt_5++;
             dp[i] = temp;
         }
+
         return dp[n];
     }
 };
