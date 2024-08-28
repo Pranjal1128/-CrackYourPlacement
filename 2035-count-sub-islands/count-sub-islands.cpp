192162b8db15def22ad96693cd394bd5d56eb781
Pranjal Naman <123015023+Pranjal1128@users.noreply.github.com>
2024-08-28 13:28:06 +0530

    Time: 203 ms (84.06%) | Memory: 93.8 MB (82.56%) - LeetSync

--- 2035-count-sub-islands/count-sub-islands.cpp
+++ 2035-count-sub-islands/count-sub-islands.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     vector<pair<int, int>> dir{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-    void dfs(int i, int j, int n, int m, bool &isIsland, vector<vector<int>> &vis, vector<vector<int>>& grid1, vector<vector<int>>& grid2) {
-        vis[i][j] = 1;
+    void dfs(int i, int j, int n, int m, bool &isIsland, vector<vector<int>>& grid1, vector<vector<int>>& grid2) {
+        grid2[i][j] = 0;
         if(!grid1[i][j]) isIsland = 0;
 
         for(auto d:dir) {
             int nx = i + d.first;
             int ny = j + d.second;
-            if(nx < 0 || ny < 0  || nx >= n || ny >= m || !grid2[nx][ny] || vis[nx][ny]) continue;
-            dfs(nx, ny, n, m, isIsland, vis, grid1, grid2);
+            if(nx < 0 || ny < 0  || nx >= n || ny >= m || !grid2[nx][ny]) continue;
+            dfs(nx, ny, n, m, isIsland, grid1, grid2);
         }
 
         return;
@@ -17,13 +17,13 @@ public:
     int countSubIslands(vector<vector<int>>& grid1, vector<vector<int>>& grid2) {
         int n = grid1.size();
         int m = grid1[0].size();
-        vector<vector<int>> vis(n, vector<int>(m, 0));
+        // vector<vector<int>> vis(n, vector<int>(m, 0));
         int cnt = 0;
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
-                if(grid2[i][j] && !vis[i][j]) {
+                if(grid2[i][j]) {
                     bool isIsland = 1;
-                    dfs(i, j, n, m, isIsland, vis, grid1, grid2);
+                    dfs(i, j, n, m, isIsland, grid1, grid2);
                     cnt += isIsland;
                 }
             }
